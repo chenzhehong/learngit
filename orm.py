85ccf65380f97c42b89137eed7aba5cd657a6d36
TomRayn <240623178@qq.com>
2018-11-26 22:29:24 +0800

    orm update2

--- orm.py
+++ orm.py
@@ -180,6 +180,51 @@ class Model(dict, metaclass=ModelMetaClass):
                 raise ValueError('Invalid limit value: %s' % str(limit))
         rs = yield from select(' '.join(sql), args)
         return [cls(**record) for record in rs]
+    
+    @classmethod
+    @asyncio.coroutine
+    def findNumber(cls, selectField, where=None, args=None):
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = yield from select(' '.join(sql), args, 1)
+        if len(rs)==0:
+            return None
+        return rs[0]['_num_']
+    
+    @classmethod
+    @asyncio.coroutine
+    def find(cls, pk):
+        sql = '%s where `%s`=?' % (cls.__select__, cls.__primary_key__)
+        rs = yield from select(sql, [pk], 1)
+        if len(rs)==0:
+            return None
+        return cls(**rs[0])
+    
+    @asyncio.coroutine
+    def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = yield from execute(self.__insert__, args)
+        if rows!=1:
+            logging.warn('fail to insert record: affected rows: %s' % rows)
+            
+    @asyncio.coroutine
+    def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = yield from execute(self.__update__, args)
+        if rows!=1:
+            logging.warn('fail to update by primary key: affected rows: %s' % rows)
+            
+    @asyncio.coroutine
+    def remove(self):
+        args = []
+        args.append(self.getValue(self.__primary_key__))
+        rows = yield from execute(self.__delete__, args)
+        if rows!=1:
+            logging.warn('fail to remove by primary key: affected rows: %s' % rows)
 
 
 
