b1e7b92e3856b10591f4bb2646761b071560c350
TomRayn <240623178@qq.com>
2018-12-02 11:33:10 +0800

    add css

--- handlers.py
+++ handlers.py
@@ -4,7 +4,7 @@ Created on Fri Nov 30 12:04:44 2018
 
 @author: chenzhehong
 """
-import asyncio
+import asyncio, time
 
 from coroweb import get, post
 
@@ -13,8 +13,13 @@ from models import User, Blog, Comment, next_id
 @get('/')
 @asyncio.coroutine
 def index(request):
-    users = yield from User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-            '__template__': 'test.html',
-            'users': users
+            '__template__': 'blogs.html',
+            'blogs': blogs
         }
