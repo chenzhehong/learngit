05440d1fd598c0b24c5e59ef60540e9bfbca64de
chenzhehong <240623178@qq.com>
2019-01-17 11:06:59 +0800

    leetcode4

---
+++ leetcode.py
@@ -0,0 +1,60 @@
+
+# coding: utf-8
+
+# In[47]:
+
+
+def getmd(nums1, nums2, cuta, cutb, starta, enda):
+    if_even = ((len(nums1) + len(nums2)) % 2 == 0)
+    if len(nums1)==0 and len(nums2)==0:
+        return 0
+    elif len(nums1)==0:
+        if if_even:
+            return (nums2[int(len(nums2)/2-1)]+nums2[int(len(nums2)/2)])/2
+        else:
+            return nums2[int(len(nums2)/2)]
+    elif len(nums2)==0:
+        if if_even:
+            return (nums1[int(len(nums1)/2-1)]+nums1[int(len(nums1)/2)])/2
+        else:
+            return nums1[int(len(nums1)/2)]
+    if cuta==0:
+        max_a = nums2[cutb-1]
+    elif cutb==0:
+        max_a = nums1[cuta-1]
+    elif nums1[cuta-1]>=nums2[cutb-1]:
+        max_a = nums1[cuta-1]
+    else:
+        max_a = nums2[cutb-1]
+    if cuta >= len(nums1):
+        min_b = nums2[cutb]
+    elif cutb >= len(nums2):
+        min_b = nums1[cuta]
+    elif nums1[cuta]<=nums2[cutb]:
+        min_b = nums1[cuta]
+    else:
+        min_b = nums2[cutb]
+    if max_a<=min_b:
+        if if_even:
+            return (max_a + min_b)/2
+        elif (cuta + cutb) > (len(nums1) + len(nums2)) / 2:
+            return max_a
+        else:
+            return min_b
+    elif nums1[cuta-1]>=nums2[cutb-1]:
+        enda = cuta
+        cuta = int((starta + cuta - 1) / 2)
+        cutb = int((len(nums1) + len(nums2) + 1) / 2) - cuta
+        return getmd(nums1, nums2, cuta, cutb, starta, enda)
+    else:
+        starta = cuta
+        cuta = int((cuta + enda + 1) / 2)
+        cutb = int((len(nums1) + len(nums2) + 1) / 2) - cuta
+        return getmd(nums1, nums2, cuta, cutb, starta, enda)
+def getMedianFromSorted(nums1, nums2):
+    size_a = len(nums1)
+    size_b = len(nums2)
+    cut_a = int((0 + size_a) / 2)
+    cut_b = int((size_a + size_b + 1) / 2) - cut_a
+    return getmd(nums1, nums2, cut_a, cut_b, 1, size_a)
+
